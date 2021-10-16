adfd9556fb3f3389b7d4f9660f0b8e22c681f261
RenukaJoshi <joshirenukab@gmail.com>
2021-10-17 03:09:12 +0530

    Passed Test string_return_sum_of_two_numbers()

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,12 +2,19 @@ package calculator;
 
 class StringCalculator {
 
-    public int add(String input) {
-    	if(input.isEmpty())
-    	{
-    		return 0;
-    	}
-        return Integer.parseInt(input);
-    }
+	public int add(String input) {
+		String[] numbers = input.split(",");
 
+		if(input.isEmpty())
+		{
+			return 0;
+		}
+		if(input.length() == 1) {
+			return Integer.parseInt(input);
+		}
+		else
+		{
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		}
+	}
 }
